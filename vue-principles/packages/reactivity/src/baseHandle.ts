951eaace5784c5c177d93975c7ebfa246e72b9e9
continue <1545620489@qq.com>
2025-10-03 16:22:27 +0800

    feature: effect依赖收集依赖出发

--- vue-principles/packages/reactivity/src/baseHandle.ts
+++ vue-principles/packages/reactivity/src/baseHandle.ts
@@ -1,3 +1,4 @@
+import { track,trigger } from "./reactiveEffect"
 export enum ReactiveFlags {
   IS_REACTIVE = '__v_isReactive'
 }
@@ -7,9 +8,15 @@ export const mutableHandlers: ProxyHandler<any> = {
     if(key===ReactiveFlags.IS_REACTIVE){
       return true
     }
+    track(target,key)
     return Reflect.get(target,key,receiver)
   },
   set(target,key,value,receiver){
-    return Reflect.set(target,key,value,receiver)
+    const oldValue= target[key]
+    const result = Reflect.set(target,key,value,receiver)
+    if(oldValue!==value){
+      trigger(target,key,value,oldValue)
+    }
+    return result
   }
 }
--- vue-principles/packages/reactivity/src/effect.ts
+++ vue-principles/packages/reactivity/src/effect.ts
@@ -1,3 +1,43 @@
-export function effect(fn){
-  console.log('effect')
+
+export function effect(fn,options={}){
+  // 保存还未执行的副作用函数
+  const _effect = new ReactiveEffect(fn,()=>{
+    _effect.run();
+  })
+  _effect.run()
+  return _effect
+}
+export let activeEffect = undefined // 当前正在执行的副作用函数
+class ReactiveEffect{
+  _trackId=0
+  deps=[] // 依赖集合
+  depLength=0
+  public active = true // 是否激活
+  constructor(public fn: () => any,public scheduler: () => void){
+    this.fn = fn
+    this.scheduler = scheduler
+  }
+  run(){
+    if(!this.active){
+      return this.fn()
+    }
+    let lastEffect = activeEffect
+    try {
+      activeEffect = this
+      return this.fn()
+    } finally  {
+      activeEffect=lastEffect
+    }
+  }
+}
+// 依赖收集
+export function trackEffect(effect,dep){
+  dep.set(effect,effect._trackId)
+  effect.deps[effect.depLength++]=dep
+}
+// 触发依赖
+export function trackEffects(dep){
+  dep.forEach((_,effectId)=>{
+    effectId.scheduler()
+  })
 }
