32673d2d40226b49936e448595caf98e601407a9
continue <1545620489@qq.com>
2025-10-07 16:10:12 +0800

    feature: runtime-dom的初识

---
+++ vue-principles/packages/runtime-dom/src/index.ts
@@ -0,0 +1,9 @@
+export * from "@vue/reactivity"
+import { nodeOps } from "./nodeOps"
+import  patchProp  from "./patchProp"
+const renderOptions=Object.assign({patchProp},nodeOps)
+export{
+  nodeOps,
+  patchProp,
+  renderOptions
+}
