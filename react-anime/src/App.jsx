354c4721f4024211d2b41cc798966645d38cc34d
continue <1545620489@qq.com>
2025-10-16 15:57:10 +0800

    feature: css动画效果

--- react-anime/src/App.jsx
+++ react-anime/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Products from './component/Products'
+import Anime from './component/anime'
 export default function App(){
   return (
     <div className="app">
       <Products />
+      <Anime />
     </div>
   )
 }
