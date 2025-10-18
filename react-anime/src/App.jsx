623bf4ede8e4bb8f1d7eeb6a963a3a50d0c1efb2
continue <1545620489@qq.com>
2025-10-18 19:49:37 +0800

    feature: 路由和redux的

--- react-anime/src/App.jsx
+++ react-anime/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Products from './component/Products'
 import Anime from './component/anime'
+import Navbar from './component/Navbar'
+
 export default function App(){
   return (
-    <div className="app">
-      <Products />
-      <Anime />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="app-content">
+        <Routes>
+          <Route path="/" element={<div className="app"><Products /></div>} />
+          <Route path="/anime" element={<Anime />} />
+          <Route path="/new" element={<New />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
---
+++ react-anime/src/component/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+export default function Navbar(){
+  return (
+    <nav className="app-navbar">
+      <div className="nav-left">
+        <Link to="/" className="nav-logo">Logo</Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/" className="nav-link">猜你喜欢</Link>
+        <Link to="/anime" className="nav-link">动画</Link>
+        <Link to="/new" className="nav-link">新番</Link>
+        {/* 未来可添加更多链接 */}
+      </div>
+    </nav>
+  )
+}
