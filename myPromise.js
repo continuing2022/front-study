59e6d7fb9e019292743929d3f3e29aa86b3160d9
continue <1545620489@qq.com>
2025-10-21 16:31:01 +0800

    feature: 异步触发then

--- myPromise.js
+++ myPromise.js
@@ -3,16 +3,24 @@ class myPromise{
     this.status="pedding"
     this.value=undefined
     this.reason=undefined
+    this.onFullfilledCallback=[]
+    this.onRejectedCallback=[]
     const resolve=(value)=>{
       if(this.status==="pedding"){
         this.value=value
         this.status="fullfilled"
+        this.onFullfilledCallback.forEach((item)=>{
+          item(this.value)
+        })
       }
     }
     const reject=(reason)=>{
       if(this.status==="pedding"){
         this.reason=reason
         this.status="rejected"
+        this.onFullfilledCallback.forEach((item)=>{
+          item(this.reason)
+        })
       }
     }
     // 执行迭代器
@@ -30,17 +38,31 @@ class myPromise{
     if(this.status==="rejected"){
       onRejected(this.reason)
     }
+    if(this.status==='pedding'){
+      this.onFullfilledCallback.push((value)=>onFullfilled(value))
+      this.onRejectedCallback.push((reason)=>onRejected(reason))
+    }
+    
   }
 }
 
 const promise=new myPromise((resolve,reject)=>{
   // resolve("成功")
   // reject("失败") 
-  throw new Error('error')
+  setTimeout(()=>{
+    resolve('异步成功')
+  },2000)
+  // throw new Error('error')
 })
 
 promise.then((value)=>{
-  console.log(value)
+  console.log("1:"+value)
 },(reason)=>{
-  console.log("error:"+reason)
+  console.log("error1:"+reason)
+})
+
+promise.then((value)=>{
+  console.log("2:"+value)
+},(reason)=>{
+  console.log("error2:"+reason)
 })
