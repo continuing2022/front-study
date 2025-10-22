2cd3d73653b2e235ae1fd2016ae1ac9f19133593
continue <1545620489@qq.com>
2025-10-22 21:06:29 +0800

    feature: 滴滴tip组件

--- tip.vue
+++ tip.vue
@@ -1,54 +1,61 @@
 <template>
-  <div class="tip" v-show="show">
+  <div class="tip" v-show="localShow">
     <span @click="onClose">{{ value }}</span>
   </div>
 </template>
 
 <script>
-  export default{
-    name:"tip",
-    props:{
-      value:{
-        type:"String",
-      },
-      show:{
-        type:"Boolean",
-        default:false
-      },
-      delay:{
-        type:"Number",
-        default:3000
-      }
+export default {
+  name: "Tip",
+  props: {
+    value: {
+      type: String
     },
-    data(){
-      return {
-      }
+    show: {
+      type: Boolean,
+      default: false
     },
-    methods:{
-      onClose() {
-        this.localShow = false
-        this.$emit('update:show', false) // 通知父组件更新
-      }
-    },
-    watch:{
-      show:function(newVal){
-        if(newVal){
-          const timer=setTimeout(()=>{
-            this.show=false
-          },delay)
-          clearTimeout(timer)
-        }
+    delay: {
+      type: Number,
+      default: 3000
+    }
+  },
+  data() {
+    return {
+      localShow: this.show,
+      timer: null
+    }
+  },
+  watch: {
+    show(newVal) {
+      this.localShow = newVal
+      if (newVal) {
+        if (this.timer) clearTimeout(this.timer)
+        this.timer = setTimeout(() => {
+          this.onClose()
+        }, this.delay)
       }
     }
+  },
+  methods: {
+    onClose() {
+      this.localShow = false
+      this.$emit('update:show', false) // 通知父组件关闭
+    }
+  },
+  beforeDestroy() {
+    clearTimeout(this.timer)
   }
+}
 </script>
+
 <style scoped>
-.tip{
-  z-index:100;
+.tip {
+  z-index: 100;
   width: 200px;
-  padding:16px;
+  padding: 16px;
   display: flex;
-  justify-items: center;
-  align-content: center;
+  justify-content: center;
+  align-items: center;
 }
 </style>
